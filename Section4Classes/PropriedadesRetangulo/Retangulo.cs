26b969319c1941e33ec068f32325461e4cbaecb0
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-10 15:37:08 -0300

    Ex1 Métodos e Classes - Propriedades do retângulo

---
+++ Section4Classes/PropriedadesRetangulo/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Globalization;
+
+namespace PropriedadesRetangulo {
+    internal class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return Largura * Altura;
+        }
+
+        public double Perimetro() {
+            return 2.0 * (Largura + Altura);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+        public override string ToString() {
+            return
+                  "\nAREA: " + Area().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPERIMETRO: " + Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDIAGONAL: " + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Section4Classes/PropriedadesRetangulo/RetanguloMain.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace PropriedadesRetangulo {
+    internal class RetanguloMain {
+        static void Main(string[] args) {
+
+            Retangulo r1 = new Retangulo();
+
+            Console.Write("Entre com a largura do retângulo: ");
+            r1.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Agora entre com a altura: ");
+            r1.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(r1);
+        }
+    }
+}
