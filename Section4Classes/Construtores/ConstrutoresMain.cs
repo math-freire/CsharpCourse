aca315fa6e73ef9f2bd0f21b17751299c5a81fed
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-10 19:47:20 -0300

    Introdução a construtores

---
+++ Section4Classes/Construtores/ConstrutoresMain.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ProdutosEmEstoque;
+class ConstrutoresMain {
+    static void Main(string[] args) {
+
+        Console.WriteLine("Entre os dados do produto:");
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+        Console.Write("Preço: $");
+        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        Produto p = new Produto(nome, preco, quantidade);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados do produto: " + p);
+
+        Console.Write("\nDigite o número de produtos a serem adicionados no estoque: ");
+        int qtd = int.Parse(Console.ReadLine());
+        p.AdicionarProduto(qtd);
+
+        Console.WriteLine("\nDados atualizados: " + p);
+
+        Console.Write("\nDigite o número de produtos a serem removidos do estoque: ");
+        qtd = int.Parse(Console.ReadLine());
+        p.RemoverProduto(qtd);
+
+        Console.WriteLine("\nDados atualizados: " + p);
+    }
+}
---
+++ Section4Classes/Construtores/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace ProdutosEmEstoque {
+    internal class Produto {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString() {
+            return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade + " unidades, Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
