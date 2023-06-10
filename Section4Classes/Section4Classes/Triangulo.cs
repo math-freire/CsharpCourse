4da33bb635e7c477540a4d9fe4ae3f36def4d654
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-10 13:45:25 -0300

    Criação do método Area para a classe Triangulo

--- Section4Classes/Section4Classes/Triangulo.cs
+++ Section4Classes/Section4Classes/Triangulo.cs
@@ -1,9 +1,16 @@
 ﻿namespace Section4Classes {
-    internal class Triangulo {
+    class Triangulo {
 
+        // Atributos da classe
         public double A;
         public double B;
         public double C;
+
+        // Métodos da classe
+        public double Area() {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p*(p-A)*(p-B)*(p-C));
+        }
 
     }
 }
