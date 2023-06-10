5aa0a80b44ce697ca8514cb8c5cb74a02ca8ada7
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-10 16:42:53 -0300

    Ex3 Métodos e Classes - Notas do aluno

---
+++ Section4Classes/Notas/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Notas {
+    internal class Aluno {
+
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double Nota() {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public string Situacao() {
+            if (Nota() < 60.0)
+                return "REPROVADO";
+            else
+                return "APROVADO";
+        }
+
+        public override string ToString() {
+            return "Nome: " + Nome +
+                "\nNota final: " + Nota().ToString("F2", CultureInfo.InvariantCulture) +
+                "\n" + Situacao();
+        }
+    }
+}
---
+++ Section4Classes/Notas/NotasMain.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Notas {
+    internal class NotasMain {
+        static void Main(string[] args) {
+            
+            Aluno a1 = new Aluno();
+
+            Console.Write("Digite o nome do aluno: ");
+            a1.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno:");
+            a1.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a1.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a1.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine(a1);
+        }
+        
+    }
+}
