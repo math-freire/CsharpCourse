7162222464bf46957c1ea9eaf55108bece9f8a63
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-10 01:54:46 -0300

    Criação da primeira classe

--- Section4Classes/Section4Classes/Program.cs
+++ Section4Classes/Section4Classes/Program.cs
@@ -3,20 +3,24 @@ using System.Globalization;
 namespace Section4Classes {
     class Program {
         static void Main(string[] args) {
-            double xA, xB, xC, yA, yB, yC;
-            Console.WriteLine("Entre com as medidas do triângulo X:");
-            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Entre com as medidas do triângulo Y:");
-            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double p = (xA + xB + xC) / 2.0;
-            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
-            p = (yA + yB + yC) / 2.0;
-            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+            Triangulo x, y;
+            x= new Triangulo();
+            y= new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
 
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
---
+++ Section4Classes/Section4Classes/Triangulo.cs
@@ -0,0 +1,9 @@
+﻿namespace Section4Classes {
+    internal class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
