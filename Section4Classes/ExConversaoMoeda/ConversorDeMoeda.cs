38af4ebafedbf0432ded9e4ab0e11420fdce2e70
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-10 19:07:36 -0300

    Ex. Métodos Estáticos - Conversor de moedas

---
+++ Section4Classes/ExConversaoMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExConversaoMoeda {
+    class ConversorDeMoeda {
+
+        static double Iof = 0.06;
+
+        // O método pode ser static pois não usa nenhum instância do objeto para realizar sua lógica
+        public static double ConverterMoeda(double cotacao, double qtdMoedaOrigem) {
+            return (qtdMoedaOrigem * cotacao) + (qtdMoedaOrigem * cotacao) * Iof;
+        }
+    }
+}
---
+++ Section4Classes/ExConversaoMoeda/ConversorMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExConversaoMoeda {
+    internal class ConversorMain {
+        static void Main(string[] args) {
+
+            Console.Write("Qual a cotação do dólar? ");
+            double cot = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dol = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais: R$" + ConversorDeMoeda.ConverterMoeda(cot, dol).ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
