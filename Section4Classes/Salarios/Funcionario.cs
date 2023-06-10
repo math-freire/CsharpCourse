be4e12af430d0296fedcc04da31c771be1759f3c
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-10 16:14:29 -0300

    Ex2 Métodos e Classes - Salário do Funcionário

---
+++ Section4Classes/Salarios/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Salarios {
+    internal class Funcionario {
+
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public void AumentarSalario(double porcentagem) {
+            SalarioBruto += (SalarioBruto * (porcentagem / 100));
+        }
+
+        public double SalarioLiquido() {
+            return SalarioBruto - Imposto;
+        }
+
+    }
+}
+
---
+++ Section4Classes/Salarios/SalariosMain.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Salarios {
+    internal class SalariosMain {
+        static void Main(string[] args) {
+
+            Funcionario f1 = new Funcionario();
+
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            f1.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f1.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: " + f1.Nome + ", Salário: $" + f1.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            f1.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.WriteLine("Dados autalizados\nNome: " + f1.Nome + ", Salário: $" + f1.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
