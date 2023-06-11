a47a4553dab01456adc31b9f93f9df828d7691d4
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-11 03:50:05 -0300

    Ex. Conta Bancária - Criando estrutura básica da classe Conta

---
+++ Section4Classes/ContaBancaria/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata;
+
+namespace ContaBancaria {
+    internal class Conta {
+
+        private string _titular;
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+
+        public string Titular {
+            get { return _titular; }
+            set {
+                if (value != null)
+                    _titular = value;
+            }
+        }
+
+        public Conta(string nome, int conta, double deposito) {
+            _titular = nome;
+            Numero = conta;
+            Saldo = deposito;
+        }
+        public Conta(String nome, int conta) {
+            _titular = nome;
+            Numero = conta;
+        }
+
+        public void Depositar(double valor) {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor) {
+            Saldo -= (5 + valor);
+        }
+
+        public override string ToString() {
+            return "Nome: " + Titular + ", número de conta: " + Numero + ", Saldo: R$" + Saldo;
+        }
+    }
+}
