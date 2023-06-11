752e3b96f78e020d3e8962ce29793f18b42e25f4
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-11 19:49:39 -0300

    Vetor de objetos (produtos)

---
+++ Section6Memoria/Vetores/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores {
+    internal class Product {
+
+        public string Name { get; set; }
+        public double Price { get; set; }  
+
+
+
+    }
+}
--- Section6Memoria/Vetores/VetoresMain.cs
+++ Section6Memoria/Vetores/VetoresMain.cs
@@ -3,6 +3,8 @@ using System.Globalization;
 namespace Vetores {
     class VetoresMain {
         static void Main(string[] args) {
+            /*
+            // Vetores aula 01
             int n = int.Parse(Console.ReadLine());
             double[] vect = new double[n];
             for (int i = 0; i < n; i++) {
@@ -14,6 +16,25 @@ namespace Vetores {
             }
             double avg = sum / n;
             Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+            */
+
+            int n = int.Parse(Console.ReadLine()); 
+            Product[] vect = new Product[n];
+
+            for(int i = 0; i < n; i++) {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price }; //Iniciação sem construtores
+            }
+
+            double sum = 0.0;
+            for(int i = 0; i < n; i++) {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("AVERAGE PRICE = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
 }
