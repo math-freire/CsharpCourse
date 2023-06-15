0baaacb718b9737535817cddb23399be79934bcf
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-15 00:55:58 -0300

    Classe implementada (método aumentar salario, atributos, ToString) e criação da lista de empregados

--- Section6Memoria/ExListOfEmployee/Employee.cs
+++ Section6Memoria/ExListOfEmployee/Employee.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace ExListOfEmployee {
     internal class Employee {
+
+        public Employee(string name, int id, double salary) {
+            Name = name;
+            Id = id;
+            Salary = salary;
+        }
 
         public int Id { get; private set; }
         public string Name { get; set; }
@@ -10,6 +17,10 @@ namespace ExListOfEmployee {
 
         public void IncreaseSalary(double percentage) {
             Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString() {
+            return $"Name: {Name}, Id: {Id}, Salary: {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
         }
 
     }
--- Section6Memoria/ExListOfEmployee/ExListOfEmployeeMain.cs
+++ Section6Memoria/ExListOfEmployee/ExListOfEmployeeMain.cs
@@ -1,7 +1,30 @@
-﻿namespace ExListOfEmployee {
+﻿using System.Globalization;
+
+namespace ExListOfEmployee {
     internal class ExListOfEmployeeMain {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int qtd = int.Parse(Console.ReadLine());
+            for (int i = 0; i < qtd; i++) {
+                Console.WriteLine($"Employee #{i+1}");
+                Console.Write("Name: ");
+                string name_aux = Console.ReadLine();
+                Console.Write("Id: ");
+                int id_aux = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Salary: ");
+                double salary_aux = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Employee p1 = new Employee(name_aux, id_aux, salary_aux);
+                list.Add(p1);
+            }
+
+            foreach (Employee p in list) {
+                Console.WriteLine(p);
+            }
+
         }
     }
 }
